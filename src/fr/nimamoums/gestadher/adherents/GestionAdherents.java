90f79e2f9e9b178a749eb93f3881f67bad6f71e1
Askyna <jacksonmellogang@gmail.com>
2022-09-15 10:50:01 +0200

    edited adherent & GestionAdherents class

--- src/fr/nimamoums/gestadher/adherents/GestionAdherents.java
+++ src/fr/nimamoums/gestadher/adherents/GestionAdherents.java
@@ -1,9 +1,22 @@
 package fr.nimamoums.gestadher.adherents;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GestionAdherents {
+
+    private static List<Adherent> adherentList = new ArrayList<>();
 
     public static void getAdherents(){
 
+    }
+
+    public static void addAdherent(Adherent adherent){
+        adherentList.add(adherent);
+    }
+
+    public static void removeAdherent(Adherent adherent){
+        adherentList.remove(adherent);
     }
 
 }
